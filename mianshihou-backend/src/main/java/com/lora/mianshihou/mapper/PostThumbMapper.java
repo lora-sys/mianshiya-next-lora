e41eede457d1c4c92532d2b4ccf255bdafa4a3fb
lora <3526039967@qq.com>
2025-10-18 12:55:59 +0800

    完成后端初始化框架搭建以及数据库设计和创建

---
+++ mianshihou-backend/src/main/java/com/lora/mianshihou/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.lora.mianshihou.mapper;
+
+import com.lora.mianshihou.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/lilora">程序员鱼皮</a>
+ * @from <a href="https://lora.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+

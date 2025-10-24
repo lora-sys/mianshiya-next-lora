8ed5e399a7484337b87882bd1d2b0e2692bb37dd
lora <3526039967@qq.com>
2025-10-24 18:31:40 +0800

    更改一些错误文件树写

--- mianshihou-backend/src/main/java/com/lora/mianshihou/mapper/QuestionMapper.java
+++ mianshihou-backend/src/main/java/com/lora/mianshihou/mapper/QuestionMapper.java
@@ -7,7 +7,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @author yanBingZhao
 * @description 针对表【question(题目)】的数据库操作Mapper
 * @createDate 2025-10-18 15:44:00
-* @Entity generator.domain.Question
+* @Entity model.Question
 */
 public interface QuestionMapper extends BaseMapper<Question> {
 
